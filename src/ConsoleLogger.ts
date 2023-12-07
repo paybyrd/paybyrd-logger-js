acb6a490a38bcaa76bf55e3cd30e3d349866eccc
Tiago Resende <tiagor87@gmail.com>
2023-12-06 21:59:17 -0300

    :art: Improve console.log

--- src/ConsoleLogger.ts
+++ src/ConsoleLogger.ts
@@ -1,16 +1,29 @@
-import { ILog, ILogger } from "./abstractions";
+import {ILog, ILogger, LogLevel} from "./abstractions";
 
 export class ConsoleLogger implements ILogger {
     flush(): Promise<void> {
         return Promise.resolve();
     }
-    
+
     log(log: ILog): void {
         const errorLog = {
             ...log,
             error: log.error?.toString()
         };
-        console.log(errorLog);
+
+        switch (log.level) {
+            case LogLevel.Trace:
+                return console.trace(errorLog);
+            case LogLevel.Debug:
+                return console.debug(errorLog);
+            case LogLevel.Warning:
+                return console.warn(errorLog);
+            case LogLevel.Error:
+            case LogLevel.Critical:
+                return console.error(errorLog);
+            default:
+                return console.log(errorLog);
+        }
     }
 
 }
